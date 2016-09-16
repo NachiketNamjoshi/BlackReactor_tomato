a5826d3f64af000a3bdcb8248422e8cb13485f91
NachiketNamjoshi <nachiketnamjoshi@gmail.com>
2016-09-16 21:06:33 +0530

    Fix Impulse for arm64

--- drivers/cpufreq/cpufreq_impulse.c
+++ drivers/cpufreq/cpufreq_impulse.c
@@ -31,12 +31,14 @@
 #include <linux/slab.h>
 #include <asm/cputime.h>
 
+#include <linux/sched/rt.h>
 #include <linux/tick.h>
 #include <linux/moduleparam.h>
 #include <linux/jiffies.h>
 #include <linux/input.h>
 #include <linux/kernel_stat.h>
 
+#include <linux/sched/rt.h>
 static int active_count;
 
 struct cpufreq_impulse_cpuinfo {
@@ -163,7 +165,7 @@ static inline u64 get_cpu_idle_time_jiffy(unsigned int cpu, u64 *wall)
 	return jiffies_to_usecs(idle_time);
 }
 
-static inline cputime64_t get_cpu_idle_time(unsigned int cpu, cputime64_t *wall)
+static inline cputime64_t get_cpu_idle_time_arm64(unsigned int cpu, cputime64_t *wall)
 {
 	u64 idle_time = get_cpu_idle_time_us(cpu, NULL);
 
@@ -200,7 +202,7 @@ static void cpufreq_impulse_timer_resched(unsigned long cpu)
 
 	spin_lock_irqsave(&pcpu->load_lock, flags);
 	pcpu->time_in_idle =
-		get_cpu_idle_time(smp_processor_id(),
+		get_cpu_idle_time_arm64(smp_processor_id(),
 				  &pcpu->time_in_idle_timestamp);
 	pcpu->cputime_speedadj = 0;
 	pcpu->cputime_speedadj_timestamp = pcpu->time_in_idle_timestamp;
@@ -246,7 +248,7 @@ static void cpufreq_impulse_timer_start(int cpu)
 	}
 
 	pcpu->time_in_idle =
-		get_cpu_idle_time(cpu, &pcpu->time_in_idle_timestamp);
+		get_cpu_idle_time_arm64(cpu, &pcpu->time_in_idle_timestamp);
 	pcpu->cputime_speedadj = 0;
 	pcpu->cputime_speedadj_timestamp = pcpu->time_in_idle_timestamp;
 	spin_unlock_irqrestore(&pcpu->load_lock, flags);
@@ -386,7 +388,7 @@ static u64 update_load(int cpu)
 	unsigned int delta_time;
 	u64 active_time;
 
-	now_idle = get_cpu_idle_time(cpu, &now);
+	now_idle = get_cpu_idle_time_arm64(cpu, &now);
 	delta_idle = (unsigned int)(now_idle - pcpu->time_in_idle);
 	delta_time = (unsigned int)(now - pcpu->time_in_idle_timestamp);
 
